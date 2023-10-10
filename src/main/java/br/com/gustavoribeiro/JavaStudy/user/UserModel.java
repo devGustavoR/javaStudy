33a471172cfc908174025287c2186a627b7911ca
devgustavor - Trampo <devgustavor@gmail.com - Trampo>
2023-10-10 16:16:11 -0300

    Add Project Lombok

--- src/main/java/br/com/gustavoribeiro/JavaStudy/user/UserModel.java
+++ src/main/java/br/com/gustavoribeiro/JavaStudy/user/UserModel.java
@@ -1,31 +1,12 @@
 package br.com.gustavoribeiro.JavaStudy.user;
 
+import lombok.Data;
+
+@Data
+
 public class UserModel {
   private String username;
   private String name;
   private int password;
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(int password) {
-    this.password = password;
-  }
-  
-  public int getPassword() {
-    return password;
-  }
 }
