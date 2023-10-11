a5d760423c5a2b38641f8eed26775ed41381824e
devgustavor - Trampo <devgustavor@gmail.com - Trampo>
2023-10-11 16:36:28 -0300

    fix:user registration

---
+++ src/main/java/br/com/gustavoribeiro/JavaStudy/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.gustavoribeiro.JavaStudy.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+  
+}
