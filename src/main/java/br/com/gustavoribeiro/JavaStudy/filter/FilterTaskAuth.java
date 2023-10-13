d35c4ef573a40ee63a2b77ee26cae7e0ead07458
devgustavor - Trampo <devgustavor@gmail.com - Trampo>
2023-10-13 17:38:13 -0300

    fix:filter completed

--- src/main/java/br/com/gustavoribeiro/JavaStudy/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gustavoribeiro/JavaStudy/filter/FilterTaskAuth.java
@@ -1,6 +1,7 @@
 package br.com.gustavoribeiro.JavaStudy.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,9 +17,27 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+
+    // Autenticação
+    var authorization = request.getHeader("Authorization");
+    var authEncoded = authorization.substring("Basic".length()).trim();
+
+    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+    var authString = new String(authDecode);
+
+    String[] credentials = authString.split(":");
+    String userName = credentials[0];
+    String password = credentials[1];
+
+    System.out.println("Authorization");
+    System.out.println(userName);
+    System.out.println(password);
+    // Validação de usuário
+
+    // Validação de senha
+
+    filterChain.doFilter(request, response);
   }
 
-  
 }
